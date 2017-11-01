09404cd49441c9f744e9168b3be6795aab6b3866
Robert <33050707+danishblunt@users.noreply.github.com>
2017-11-01 16:10:37 +0100

    Add files via upload

---
+++ BookingAppNew/js/app.js
@@ -0,0 +1,24 @@
+(function(){
+ var app = angular.module('booking', [ ]);
+    
+ app.controller('BookingController', function($http){
+	var vm = this;
+	var url = "data/data.json";
+	$http.get(url).success( function(response) {
+		vm.products = response.rooms;
+		console.log(response.rooms);
+	});
+ });
+    
+ app.controller('CalendarController', function() {
+     this.tab = 0;
+     
+     this.selectTab = function(setTab) {
+         this.tab = setTab;
+     };
+     
+     this.isSelected = function(checkTab) {
+        return this.tab === checkTab;
+     };
+ });  
+ })(); 
